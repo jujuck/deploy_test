fe1ae76905d12d7029711a0a616bdcccd56f16e2
Julien Richard [RMT - JS] Wild Code School <33219171+jujuck@users.noreply.github.com>
2023-12-14 11:53:32 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,19 @@
-import Counter from "./components/Counter";
-import logo from "./assets/logo.svg";
+import react, {useEffect, useState} form 'react;
 
 import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/items`)
+      .then((res) => res.json())
+      .then((json) => setItems(json))
+      .catch((err) => console.error(err));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React !</p>
-
-        <Counter />
-
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      {items.map((item) => <h2>{item.title}</h2>)}
     </div>
   );
 }
